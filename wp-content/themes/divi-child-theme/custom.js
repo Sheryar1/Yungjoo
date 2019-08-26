9c8ac4fff4acc23c8cb572dac84a4ff3edc53794
kanwalmajeed3 <kanwal.majeed@thechakor.com>
2019-08-27 00:48:27 +0500

    pricing page

--- wp-content/themes/divi-child-theme/custom.js
+++ wp-content/themes/divi-child-theme/custom.js
@@ -22,19 +22,5 @@ jQuery(document).ready(function( $ ) {
 	jQuery('#datetimepicker7').datetimepicker();
 });
 
-jQuery('#free_package_btn').click(function() {
-    var f_start_date = jQuery( '#free_date_timepicker_start' ).val();
-	var f_end_date = jQuery( '#free_date_timepicker_end' ).val();
-    if(f_start_date != "" || f_end_date != ""){  
 
-     var free_url = jQuery(this).attr('href');
-    	var free_new_url = free_url.replace("f_s_date", f_start_date);
-    	var free_neww_url = free_new_url.replace("f_e_date", f_end_date);  
-    	var free_final_url = jQuery('#free_package_btn').attr("href", free_neww_url);
-    }
-    else{
-    	jQuery('.free_error_msg').show();
-      return false;
-    }
-  });
   
