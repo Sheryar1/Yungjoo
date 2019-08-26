ff0d937d5a5c9ef5d85606c344bf7dc5aa2544bf
kanwalmajeed3 <kanwal.majeed@thechakor.com>
2019-08-26 23:39:49 +0500

    pricing page

--- wp-content/themes/divi-child-theme/custom.js
+++ wp-content/themes/divi-child-theme/custom.js
@@ -1,6 +1,19 @@
 jQuery(document).ready(function( $ ) {
-	jQuery('#datetimepicker').datetimepicker();
-	jQuery('#datetimepicker1').datetimepicker();
+	var minDateTime =  new Date();
+	minDateTime.setHours(minDateTime.getHours()-1);
+	jQuery('#free_date_timepicker_start').datetimepicker({
+		minDate: 0,
+		minDateTime: minDateTime,
+		todayButton: true,
+		
+	   });
+	jQuery("#free_date_timepicker_start").change(function(){
+		var start_datet =  jQuery('#free_date_timepicker_start').val();
+		var hours = start_datet.getHours();
+		alert(hours);
+		jQuery('#free_date_timepicker_end').val(start_datet);
+	});   
+	  
 	jQuery('#datetimepicker2').datetimepicker();
 	jQuery('#datetimepicker3').datetimepicker();
 	jQuery('#datetimepicker4').datetimepicker();
@@ -8,3 +21,20 @@ jQuery(document).ready(function( $ ) {
 	jQuery('#datetimepicker6').datetimepicker();
 	jQuery('#datetimepicker7').datetimepicker();
 });
+
+jQuery('#free_package_btn').click(function() {
+    var f_start_date = jQuery( '#free_date_timepicker_start' ).val();
+	var f_end_date = jQuery( '#free_date_timepicker_end' ).val();
+    if(f_start_date != "" || f_end_date != ""){  
+
+     var free_url = jQuery(this).attr('href');
+    	var free_new_url = free_url.replace("f_s_date", f_start_date);
+    	var free_neww_url = free_new_url.replace("f_e_date", f_end_date);  
+    	var free_final_url = jQuery('#free_package_btn').attr("href", free_neww_url);
+    }
+    else{
+    	jQuery('.free_error_msg').show();
+      return false;
+    }
+  });
+  
--- wp-content/themes/divi-child-theme/functions.php
+++ wp-content/themes/divi-child-theme/functions.php
@@ -28,3 +28,44 @@ function divichild_enqueue_scripts() {
 	wp_enqueue_script('custom_js');
 }
 add_action( 'wp_enqueue_scripts', 'divichild_enqueue_scripts' );
+
+// Add custom note as custom cart item data
+add_filter( 'woocommerce_add_cart_item_data', 'get_custom_product_data', 30, 2 );
+function get_custom_product_data( $cart_item_data, $product_id ){
+    if ( isset($_GET['f_start_date']) && ! empty($_GET['f_start_date']) || isset($_GET['f_end_date']) && ! empty($_GET['f_end_date'])) {
+        $cart_item_data['free_pkg_start'] = sanitize_text_field( $_GET['f_start_date'] );
+    
+        $cart_item_data['free_pkg_end'] = sanitize_text_field( $_GET['f_end_date'] );
+		
+        //$cart_item_data['unique_key'] = md5( microtime().rand() );
+    }
+    return $cart_item_data;
+}
+
+
+// Display note in cart and checkout pages as cart item data - Optional
+add_filter( 'woocommerce_get_item_data', 'display_custom_item_data', 10, 2 );
+function display_custom_item_data( $cart_item_data, $cart_item ) {
+    if ( isset( $cart_item['free_pkg_start'] ) ||  isset( $cart_item['free_pkg_end'] )){
+        $cart_item_data[] = array(
+            'name' => "Start Time",
+            'value' =>   $cart_item['free_pkg_start'],
+
+        );
+		$cart_item_data[] = array(
+            'name' => "End Time",
+            'value' =>   $cart_item['free_pkg_end'],
+        );
+    }
+	
+    return $cart_item_data;
+}
+
+// Save and display product note in orders and email notifications (everywhere)
+add_action( 'woocommerce_checkout_create_order_line_item', 'add_custom_data_order_item_meta', 20, 4 );
+function add_custom_data_order_item_meta( $item, $cart_item_key, $values, $order ) {
+    if ( isset( $values['free_pkg_start'] ) ||  isset( $values['free_pkg_end'] ) ){
+        $item->update_meta_data( 'Start Time',  $values['free_pkg_start'] );
+		$item->update_meta_data( 'End Time',  $values['free_pkg_end'] );
+	}
+}
