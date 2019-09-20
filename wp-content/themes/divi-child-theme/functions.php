a833de0ecb0edbc41bfed5ba9ea6fd543912239e
kanwalmajeed3 <kanwal.majeed@thechakor.com>
2019-09-20 20:40:10 +0500

    restriction

--- wp-content/themes/divi-child-theme/functions.php
+++ wp-content/themes/divi-child-theme/functions.php
@@ -895,3 +895,19 @@ function disable_dates(){
 	}
 	die();
 }
+function my_login_redirect( $redirect_to, $request, $user ) {
+    //is there a user to check?
+    global $user;
+    if ( isset( $user->roles ) && is_array( $user->roles ) ) {
+
+        if ( in_array( 'customer', $user->roles ) ) {
+            // redirect them to the default place
+            return '/pricing';
+        } else {
+            return home_url();
+        }
+    } else {
+        return $redirect_to;
+    }
+}
+add_filter( 'login_redirect', 'my_login_redirect', 10, 3 );
